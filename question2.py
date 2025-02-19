bf65117b98507d64cac5cde71414192680af0617
Gokila-K2593 <gokilakannan2003@gmail.com>
2025-02-19 15:58:19 +0530

    output

---
+++ question2.py
@@ -0,0 +1,9 @@
+import re
+from collections import Counter
+def most_common_word(text):
+    words = re.findall(r'\b\w+\b', text.lower())
+    most_common = Counter(words).most_common(1)[0][0]
+    return most_common
+text = input("Enter a paragraph: ")
+print("The most common word is:", most_common_word(text))
+
